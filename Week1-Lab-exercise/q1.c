20f2d58fd56641ac294e6cd30a1ec91b65222618
G. Bala Krishna <104557329+worldofbk@users.noreply.github.com>
2022-08-18 10:31:26 +0530

    Add files via upload

---
+++ Week1-Lab-exercise/q1.c
@@ -0,0 +1,62 @@
+
+#include <stdio.h>
+
+void main()
+{
+    int state=0;
+    int i=0;
+    char token, input[20];
+    printf("Enter input string \t :");
+    scanf("%s",input);
+    
+    while((token=input[i++])!='\0'){
+        switch(state){
+            case 0: if(token=='a')
+                        state=1;
+                    else if(token=='b')
+                        state=3;
+                    else{
+                        printf("Invalid token");
+                        //exit(0);
+                    }
+                    break;
+            
+            case 1: if(token=='a')
+                        state=0;
+                    else if(token=='b')
+                        state=2;
+                    else{
+                        printf("Invalid token");
+                        //exit(0);
+                    }
+                    break;   
+            
+            case 2: if(token=='a')
+                        state=3;
+                    else if(token=='b')
+                        state=1;
+                    else{
+                        printf("Invalid token");
+                        //exit(0);
+                    }
+                    break;
+            
+            case 3: if(token=='a')
+                        state=2;
+                    else if(token=='b')
+                        state=3;
+                    else{
+                        printf("Invalid token");
+                        //exit(0);
+                    }
+                    break;    
+        }
+        
+    }
+    if(state==0){
+        printf("String accepted");
+    }
+    else{
+        printf("String not accepted");
+    }
+}
